39b7579612e06d8e8a95b51d0c21723505a7a343
felipeCarnevale <felipecarnevale@id.uff.br>
2020-09-29 14:11:22 -0300

    testando o servidor php

---
+++ welcome.php
@@ -0,0 +1,11 @@
+<html>
+<body>
+  <div >
+    <h1><b>Pagina para teste da configuração do servidor php</b></h1>
+  </div>
+  <?php
+  $nome = $_POST["user"];
+
+  echo "O nome digitado foi : ", $nome;
+  ?>
+</body>
