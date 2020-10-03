0bb329d48a076541d8eb4f89dafaea1c8aa903a2
felipeCarnevale <felipecarnevale@id.uff.br>
2020-10-03 12:41:57 -0300

    conexão com db implementada

--- welcome.php
+++ welcome.php
@@ -4,8 +4,24 @@
     <h1><b>Pagina para teste da configuração do servidor php</b></h1>
   </div>
   <?php
-  $nome = $_POST["user"];
+  $server = "localhost";
+  $username = "root";
+  $password = "";
+  $conn = new mysqli($server,$username,$password);
+  if($conn->connect_error){
+    die("Connection failed: " . $conn->connect_error);
+  }
+  $conn -> select_db("loginProgweb");
 
-  echo "O nome digitado foi : ", $nome;
+  $nome = $_POST["user"];
+  $senha = $_POST["senha"];
+  $sql = "select * from users where usuario = " . "'" . $nome . "'" ." and senha = ". "'" . $senha . "'";
+  $resultado = $conn->query($sql);
+  if($resultado->num_rows > 0){
+    echo "Usuario logado com sucesso";
+  }
+  else{
+    echo "Usuario ou senha invalidos";
+  }
   ?>
 </body>
